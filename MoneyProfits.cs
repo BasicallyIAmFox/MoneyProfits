cb1f0e5f9c96ae89d3240a15b67d51a3af3558d0
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2023-08-28 10:19:03 +0500

    Revert "added support for magic storage and bettercrafting"
    This reverts commit 7d948b9b403aebb0d93365f392ebfb2809eb4f51.

--- MoneyProfits.cs
+++ MoneyProfits.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using MagicStorage.Common.Systems;
-using MagicStorage.UI;
-using MagicStorage.UI.States;
 using Microsoft.Xna.Framework;
-using MonoMod.Cil;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -21,91 +16,54 @@ public sealed partial class MoneyProfits : Mod {
 		public sealed override bool InstancePerEntity => true;
 		protected sealed override bool CloneNewInstances => false;
 
-		public sealed override void ModifyTooltips(Item item, List<TooltipLine> lines) {
-			int index = RecipeIndex;
-			if (HasMagicStorage) {
-				TryAssignCache_MagicStorage(ref index);
-			}
-
-			if (index != -1) {
-				lines.Add(new TooltipLine(MoneyProfits.Instance, "Profit",
+		public sealed override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+			if (RecipeIndex != -1) {
+				tooltips.Add(new TooltipLine(
+					MoneyProfits.Instance, "Profit",
 					Language.GetTextValue(
 						"Mods.MoneyProfits.ProfitText",
-						Cache[index].ToString())) {
+						Cache[Main.availableRecipe[RecipeIndex]].ToString()
+					)) {
 					OverrideColor = Color.Gold
 				});
 			}
 		}
-
-		[JITWhenModsEnabled("MagicStorage")]
-		[MethodImpl(MethodImplOptions.NoInlining)]
-		private static void TryAssignCache_MagicStorage(ref int value) {
-			var zone = (NewUISlotZone)Holder.MagicStorage_recipeHeaderZone.GetValue((CraftingUIState)MagicUI.craftingUI);
-			var recipe = Holder.MagicStorage_selectedRecipe();
-
-			if (zone != null && recipe != null) {
-				value = recipe.RecipeIndex;
-			}
-		}
 	}
-
 	private sealed class ProfitCacheLoader : ModSystem {
 		public sealed override void PostSetupRecipes() {
-			Cache = Main.recipe.Take(Recipe.numRecipes).Select(x => new ProfitCache(x)).ToArray();
-		}
-
-		public sealed override void Unload() {
-			if (Cache != null)
-				Array.Clear(Cache);
+			Cache = Main.recipe[..Recipe.numRecipes].Select(x => new ProfitCache(x)).ToArray();
 		}
 	}
-
-	internal static ReflectionHolder Holder { get; private set; }
 
 	public static MoneyProfits Instance { get; private set; }
 	public static ProfitCache[] Cache { get; private set; }
-
-	public static bool HasBetterCrafting { get; private set; }
-	public static bool HasMagicStorage { get; private set; }
 
 	public MoneyProfits() {
 		Instance = this;
 	}
 
 	public sealed override void Load() {
-		IL_Main.DrawInventory += (il) => {
-			try {
-				AssignRecipeIndexesToWindow(new ILCursor(il));
-			}
-			catch {
-				MonoModHooks.DumpIL(Instance, il);
-			}
-		};
-		IL_Main.HoverOverCraftingItemButton += (il) => {
-			try {
-				AssignRecipeIndexesToList(new ILCursor(il));
-			}
-			catch {
-				MonoModHooks.DumpIL(Instance, il);
-			}
-		};
-
-		HasBetterCrafting = ModLoader.HasMod("BetterCrafting");
-		HasMagicStorage = ModLoader.HasMod("MagicStorage");
-
-		Holder = new();
-		Holder.Load();
+#if !TML_2022_09
+		IL_Main.DrawInventory += AssignRecipeIndexesToWindow;
+		IL_Main.HoverOverCraftingItemButton += AssignRecipeIndexesToList;
+#else
+		IL.Terraria.Main.DrawInventory += AssignRecipeIndexesToWindow;
+		IL.Terraria.Main.HoverOverCraftingItemButton += AssignRecipeIndexesToList;
+#endif
 	}
 
 	public sealed override void Unload() {
-		Holder?.Unload();
-		Holder = null;
+#if !TML_2022_09
+		IL_Main.DrawInventory -= AssignRecipeIndexesToWindow;
+		IL_Main.HoverOverCraftingItemButton -= AssignRecipeIndexesToList;
+#else
+		IL.Terraria.Main.DrawInventory -= AssignRecipeIndexesToWindow;
+		IL.Terraria.Main.HoverOverCraftingItemButton -= AssignRecipeIndexesToList;
+#endif
+
+		if (Cache != null)
+			Array.Clear(Cache);
 
 		Instance = null;
-	}
-
-	private static void TryAssignRecipeIndex(Item item, int index) {
-		if (item.TryGetGlobalItem<ShowItemProfitability>(out var result))
-			result.RecipeIndex = index;
 	}
 }
